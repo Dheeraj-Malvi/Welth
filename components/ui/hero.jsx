3fb656a17910e047fba91bcbae9d5e6ff141fdcc
Deepak-Mandal <deepak379mandal@gmail.com>
2025-04-20 18:07:39 +0530

    added sections- "How It Works, What Our Users Say"

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
   return (
     <div className="pb-20 px-3">
       <div className="container mx-auto text-center">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient gradient-title">
           Manage Your Finances <br /> with Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
